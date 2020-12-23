230cd2e28a6f5876cfc98265aa9776971477564d
Bapi Dey <devbapidey@gmail.com>
2020-12-23 10:40:15 +0530

    This is single Action Controller

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Hello', \App\Http\Controllers\SingleActionController::class); // Single Access Controller
